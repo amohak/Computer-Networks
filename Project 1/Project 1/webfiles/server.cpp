38d679435abb5ae6f36b2cc6b30ed0db068f7655
mohakavi <amohak@iitk.ac.in>
2016-08-19 14:22:23 +0530

    Cleaned Code with commented code

--- Project 1/Project 1/webfiles/server.cpp
+++ Project 1/Project 1/webfiles/server.cpp
@@ -86,113 +86,157 @@ int main(int argc, char * argv[])
 		{
 			cout << "Inside the child of Connection #" << connection_count << "\n";
 
-				char *connection_status;
-				int content_length, retval, type;
-				char *temp_type;
-				struct stat size_info, type_info;
-				char *response_header = (char *)malloc(MAX_BUFFER_SIZE);
+			char *connection_status;
+			int content_length, retval, type;
+			char *temp_type;
+			struct stat size_info, type_info, temp_file_info;
+			char *response_header = (char *)malloc(MAX_BUFFER_SIZE);
 
-				char *requestmessage = (char *)malloc(MAX_BUFFER_SIZE);
+			char *requestmessage = (char *)malloc(MAX_BUFFER_SIZE);
 
-				if(recv(new_s,requestmessage,MAX_BUFFER_SIZE,0) <= 0){					// receiving filename and checking for live client
+			if(recv(new_s,requestmessage,MAX_BUFFER_SIZE,0) <= 0){					// receiving filename and checking for live client
+				exit(1);
+			}
+
+			int l = strlen(requestmessage);
+			requestmessage[l] = '\0'; 
+
+			ParsedRequest *req = ParsedRequest_create();
+			if ((retval = ParsedRequest_parse(req, requestmessage, l)) < 0) {
+				if(retval == -2)
+				{
+					status_line("HTTP/1.1",501,response_header);
+				}
+
+				else
+				{
+					status_line("HTTP/1.1",400,response_header);
+				}
+
+				retval = abs(retval) - 1;
+
+				prepare_response("keep-alive",strlen(ParsingFailed[retval]),"txt",response_header);
+				send_new(new_s,response_header,strlen(response_header));
+				send_new(new_s,ParsingFailed[retval],strlen(ParsingFailed[retval]));
+				continue;
+			}
+
+			ParsedHeader header;
+			for(int i=0;i<req->headersused;i++)
+			{
+				header = *(req->headers+i);
+				if(strcmp(LowerCase(header.key),"connection")==0)
+				{
+					connection_status = (char *)malloc(strlen(header.value));
+					strcpy(connection_status,header.value);
+				}
+			}
+
+			if(req->path[strlen(req->path)-1]=='/')				// trim the terminal slash character
+				req->path[strlen(req->path)-1] = '\0';
+			
+			type = stat(req->path,&type_info);
+
+			int file_descriptor;
+
+			int f = open(req->path,O_RDONLY);
+
+			if(type != 0)
+			{
+				// perror("File/Directory not found");
+				status_line(req->version,404,response_header);
+				prepare_response(connection_status,strlen(BadRequestMessage),"txt",response_header);
+				send_new(new_s,response_header,strlen(response_header));
+				send_new(new_s,BadRequestMessage,strlen(BadRequestMessage));
+			}
+			
+			else
+			{
+				status_line(req->version,200,response_header);
+
+				if(S_ISREG(type_info.st_mode))
+				{
+					file_descriptor = f;
+					// printf("File exists\n");
+					// status_line(req->version,200,response_header);
+
+					// if(fstat(file_descriptor,&size_info) < 0)
+					// {
+					// 	perror("Error in accessing file stats");
+					// 	exit(1);
+					// }
+
+					// content_length = size_info.st_size;
+					temp_type = extract_type(req->path);
+					// string content_type = MediaType(temp_type);
+
+					// prepare_response(connection_status,content_length,content_type,response_header);				
+					// send_new(new_s,response_header,strlen(response_header));
+					// sendfile_new(new_s,f,content_length);
+				}
+
+				if(S_ISDIR(type_info.st_mode))
+				{
+					// status_line(req->version,200,response_header);
+
+					DIR * dir = opendir(req->path);
+					FILE *fp = fopen("directory_list.html","w");
+					
+					if(fp == NULL)
+						exit(1);
+					
+					fprintf(fp,"%s",htmlheader.c_str());
+					list_dir_file(dir,fp,req->path);
+					fprintf(fp,"%s",htmlfooter.c_str());
+					fclose(fp);
+
+					file_descriptor = open("directory_list.html",O_RDONLY);
+
+					// if(fstat(file_descriptor,&size_info) < 0)
+					// {
+					// 	perror("Error in accessing file stats");
+					// 	exit(1);
+					// }
+
+					temp_type = "html";
+
+					// string content_type = MediaType("html");
+
+					// prepare_response(connection_status,content_length,content_type,response_header);
+					// send_new(new_s,response_header,strlen(response_header));
+					// sendfile_new(new_s,f1,content_length);
+
+					// int ret = remove("directory_list.html");
+					// if(ret != 0) 
+					// 	exit(1);
+				}
+
+				if(fstat(file_descriptor,&size_info) < 0)
+				{
+					perror("Error in accessing file stats");
 					exit(1);
 				}
 
-				int l = strlen(requestmessage);
-				requestmessage[l] = '\0'; 
+				content_length =  size_info.st_size;
+				string content_type = MediaType(temp_type);
 
-				ParsedRequest *req = ParsedRequest_create();
-				if ((retval = ParsedRequest_parse(req, requestmessage, l)) < 0) {
-					if(retval == -2)
-					{
-						status_line("HTTP/1.1",501,response_header);
-					}
+				prepare_response(connection_status,content_length,content_type,response_header);				
+				send_new(new_s,response_header,strlen(response_header));
+				sendfile_new(new_s,file_descriptor,content_length);
 
-					else
-					{
-						status_line("HTTP/1.1",400,response_header);
-					}
+				if(S_ISDIR(type_info.st_mode))
+				{
+					int ret = remove("directory_list.html");
+					if(ret != 0) 
+						exit(1);
+				}
 
-					retval = abs(retval) - 1;
-
-					prepare_response("keep-alive",strlen(ParsingFailed[retval]),"txt",response_header);
-					send_new(new_s,response_header,strlen(response_header));
-					send_new(new_s,ParsingFailed[retval],strlen(ParsingFailed[retval]));
+				if(strcmp(connection_status,"close")==0)
+				{
 					close(new_s);
-					exit(0);
+					break;
 				}
-
-				ParsedHeader header;
-				for(int i=0;i<req->headersused;i++)
-				{
-					header = *(req->headers+i);
-					if(strcmp(LowerCase(header.key),"connection")==0)
-					{
-						connection_status = (char *)malloc(strlen(header.value));
-						strcpy(connection_status,header.value);
-					}
-				}
-
-				if(req->path[strlen(req->path)-1]=='/')				// trim the terminal slash character
-					req->path[strlen(req->path)-1] = '\0';
-				
-				type = stat(req->path,&type_info);
-
-				int f = open(req->path,O_RDONLY);
-
-				if(type != 0)
-				{
-					// perror("File/Directory not found");
-					status_line(req->version,404,response_header);
-					prepare_response(connection_status,strlen(BadRequestMessage),"txt",response_header);
-					send_new(new_s,response_header,strlen(response_header));
-					send_new(new_s,BadRequestMessage,strlen(BadRequestMessage));
-				}
-				else
-				{
-					if(S_ISREG(type_info.st_mode))
-					{
-						// printf("File exists\n");
-						status_line(req->version,200,response_header);
-
-						if(fstat(f,&size_info) < 0)
-						{
-							perror("Error in accessing file stats");
-							close(new_s);
-							exit(1);
-						}
-
-						content_length = size_info.st_size;
-						temp_type = extract_type(req->path);
-						string content_type = MediaType(temp_type);
-
-						prepare_response(connection_status,content_length,content_type,response_header);				
-						
-						send_new(new_s,response_header,strlen(response_header));
-						sendfile_new(new_s,f,content_length);
-
-						close(f);
-					}
-
-					if(S_ISDIR(type_info.st_mode))
-					{
-						status_line(req->version,200,response_header);
-
-						char directory_list[8092];
-						memset(directory_list,'\0',8092);
-						DIR * dir = opendir(req->path);
-						
-						strcat(directory_list,htmlheader.c_str());
-						list_dir(dir,directory_list,req->path);
-						strcat(directory_list,htmlfooter.c_str());
-
-						content_length = strlen(directory_list);
-						string content_type = MediaType("html");
-						prepare_response(connection_status,content_length,content_type,response_header);
-						send_new(new_s,response_header,strlen(response_header));
-						send_new(new_s,directory_list,strlen(directory_list));
-					}
-				}
+			}
 		}
 	}
 	return 0;
