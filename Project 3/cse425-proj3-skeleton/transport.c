2330779d80d2f09e1f95833191f266c432e759c0
mohakavi <amohak@iitk.ac.in>
2016-09-29 05:08:20 +0530

    Handshake done

--- Project 3/cse425-proj3-skeleton/transport.c
+++ Project 3/cse425-proj3-skeleton/transport.c
@@ -21,18 +21,22 @@
 #include "transport.h"
 
 
-enum { CSTATE_ESTABLISHED };    /* you should have more states */
+enum { 	CSTATE_ESTABLISHED = 1,
+		LISTEN = 2,
+		SYN_SENT = 3,
+		SYN_RECD = 4  
+	 };    /* you should have more states */  /* I have it already */
 
 
 /* this structure is global to a mysocket descriptor */
 typedef struct
 {
-    bool_t done;    /* TRUE once connection is closed */
+	bool_t done;    /* TRUE once connection is closed */
 
-    int connection_state;   /* state of the connection (established, etc.) */
-    tcp_seq initial_sequence_num;
+	int connection_state;   /* state of the connection (established, etc.) */
+	tcp_seq initial_sequence_num;
 
-    /* any other connection-wide global variables go here */
+	/* any other connection-wide global variables go here */
 } context_t;
 
 
@@ -46,41 +50,114 @@ static void control_loop(mysocket_t sd, context_t *ctx);
  */
 void transport_init(mysocket_t sd, bool_t is_active)
 {
-    context_t *ctx;
+	context_t *ctx;
 
-    ctx = (context_t *) calloc(1, sizeof(context_t));
-    assert(ctx);
+	ctx = (context_t *) calloc(1, sizeof(context_t));
+	assert(ctx);
 
-    generate_initial_seq_num(ctx);
+	generate_initial_seq_num(ctx);
 
-    /* XXX: you should send a SYN packet here if is_active, or wait for one
-     * to arrive if !is_active.  after the handshake completes, unblock the
-     * application with stcp_unblock_application(sd).  you may also use
-     * this to communicate an error condition back to the application, e.g.
-     * if connection fails; to do so, just set errno appropriately (e.g. to
-     * ECONNREFUSED, etc.) before calling the function.
-     */
-    ctx->connection_state = CSTATE_ESTABLISHED;
-    stcp_unblock_application(sd);
+	STCPHeader *synPacket, *synAckPacket, *ackPacket, *inPacket = (STCPHeader *) calloc(1,sizeof(STCPHeader));
+	// STCPHeader *synAckPacket = (STCPHeader *) calloc(1,sizeof(STCPHeader));
+	// STCPHeader *ackPacket = (STCPHeader *) calloc(1,sizeof(STCPHeader));
+	
+	if(is_active)
+	{
 
-    control_loop(sd, ctx);
+		synPacket->th_seq = ctx->initial_sequence_num;
+		synPacket->th_off = 5;
+		synPacket->th_flags = TH_SYN;
+		stcp_network_send(sd,synPacket,sizeof(synPacket),NULL);
 
-    /* do any cleanup here */
-    free(ctx);
+		ctx->connection_state = SYN_SENT;
+
+		stcp_network_recv(sd,inPacket,sizeof(inPacket));
+
+		// if(synAckPacket->th_ack == ctx->initial_sequence_num + 1)
+		if(inPacket->th_flags & TH_SYN)
+		{
+			ctx->connection_state = SYN_RECD;
+
+			synAckPacket->th_ack = inPacket->th_seq + 1;
+			synAckPacket->th_seq = ctx->initial_sequence_num;
+			synAckPacket->th_off = 5;
+			synAckPacket->th_flags = (TH_SYN | TH_ACK);
+
+			stcp_network_send(sd,synAckPacket,sizeof(synAckPacket),NULL);
+
+			stcp_network_recv(sd,inPacket,sizeof(inPacket));
+		}
+		
+		if((inPacket->th_flags & (TH_SYN | TH_ACK))) 
+		{
+			ackPacket->th_seq = ctx->initial_sequence_num + 1;
+			ackPacket->th_ack = inPacket->th_seq + 1;
+			ackPacket->th_off = 5;
+			ackPacket->th_flags = TH_ACK;
+
+			stcp_network_send(sd,ackPacket,sizeof(ackPacket),NULL);
+		}
+		else
+		{
+			//error handling
+		}
+	}
+
+	else
+	{
+		ctx->connection_state = LISTEN;
+
+		stcp_network_recv(sd,synPacket,sizeof(synPacket));
+		if(synPacket->th_flags != TH_SYN)
+		{
+			//error handling
+		}
+		else
+		{
+			synAckPacket->th_ack = synPacket->th_seq + 1;
+			synAckPacket->th_seq = ctx->initial_sequence_num;
+			synAckPacket->th_off = 5;
+			synAckPacket->th_flags = (TH_SYN | TH_ACK);
+
+			stcp_network_send(sd,synAckPacket,sizeof(synAckPacket),NULL);
+
+			ctx->connection_state = SYN_RECD;
+
+			stcp_network_recv(sd,ackPacket,sizeof(ackPacket));
+			if(ackPacket->th_flags != TH_ACK || ackPacket->th_ack != ctx->initial_sequence_num + 1)
+			{
+				//error handling
+			}
+		}
+	}
+	/* XXX: you should send a SYN packet here if is_active, or wait for one
+	 * to arrive if !is_active.  after the handshake completes, unblock the
+	 * application with stcp_unblock_application(sd).  you may also use
+	 * this to communicate an error condition back to the application, e.g.
+	 * if connection fails; to do so, just set errno appropriately (e.g. to
+	 * ECONNREFUSED, etc.) before calling the function.
+	 */
+	ctx->connection_state = CSTATE_ESTABLISHED;
+	stcp_unblock_application(sd);
+
+	control_loop(sd, ctx);
+
+	/* do any cleanup here */
+	free(ctx);
 }
 
 
 /* generate random initial sequence number for an STCP connection */
 static void generate_initial_seq_num(context_t *ctx)
 {
-    assert(ctx);
+	assert(ctx);
 
 #ifdef FIXED_INITNUM
-    /* please don't change this! */
-    ctx->initial_sequence_num = 1;
+	/* please don't change this! */
+	ctx->initial_sequence_num = 1;
 #else
-    /* you have to fill this up */
-    /*ctx->initial_sequence_num =;*/
+	/* you have to fill this up */
+	/*ctx->initial_sequence_num =;*/
 #endif
 }
 
@@ -94,26 +171,26 @@ static void generate_initial_seq_num(context_t *ctx)
  */
 static void control_loop(mysocket_t sd, context_t *ctx)
 {
-    assert(ctx);
-    assert(!ctx->done);
+	assert(ctx);
+	assert(!ctx->done);
 
-    while (!ctx->done)
-    {
-        unsigned int event;
+	while (!ctx->done)
+	{
+		unsigned int event;
 
-        /* see stcp_api.h or stcp_api.c for details of this function */
-        /* XXX: you will need to change some of these arguments! */
-        event = stcp_wait_for_event(sd, 0, NULL);
+		/* see stcp_api.h or stcp_api.c for details of this function */
+		/* XXX: you will need to change some of these arguments! */
+		event = stcp_wait_for_event(sd, 0, NULL);
 
-        /* check whether it was the network, app, or a close request */
-        if (event & APP_DATA)
-        {
-            /* the application has requested that data be sent */
-            /* see stcp_app_recv() */
-        }
+		/* check whether it was the network, app, or a close request */
+		if (event & APP_DATA)
+		{
+			/* the application has requested that data be sent */
+			/* see stcp_app_recv() */
+		}
 
-        /* etc. */
-    }
+		/* etc. */
+	}
 }
 
 
@@ -132,15 +209,15 @@ static void control_loop(mysocket_t sd, context_t *ctx)
  */
 void our_dprintf(const char *format,...)
 {
-    va_list argptr;
-    char buffer[1024];
+	va_list argptr;
+	char buffer[1024];
 
-    assert(format);
-    va_start(argptr, format);
-    vsnprintf(buffer, sizeof(buffer), format, argptr);
-    va_end(argptr);
-    fputs(buffer, stdout);
-    fflush(stdout);
+	assert(format);
+	va_start(argptr, format);
+	vsnprintf(buffer, sizeof(buffer), format, argptr);
+	va_end(argptr);
+	fputs(buffer, stdout);
+	fflush(stdout);
 }
 
 
